98f54f195d7cd1b4626cd52c1443e5afdd585dc0
Camilo Jimenez <jucajigue+developer@gmail.com>
2023-06-16 15:13:59 -0500

    feat: add privates routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,24 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
 
-import { NavBar } from "../ui";
+import { PrivateRoutes } from "./PrivateRoutes";
 
 const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoutes>
+              <HeroesRoutes />
+            </PrivateRoutes>
+          }
+        />
       </Routes>
     </>
   );
